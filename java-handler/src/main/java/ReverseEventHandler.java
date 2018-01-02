42b9f174c5ce618a6b2f648d766fb5945ccac1a0
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-02 15:22:50 +0200

    Java reverse handler

---
+++ java-handler/src/main/java/ReverseEventHandler.java
@@ -0,0 +1,15 @@
+import io.nuclio.Context;
+import io.nuclio.Event;
+import io.nuclio.EventHandler;
+import io.nuclio.Response;
+
+
+public class ReverseEventHandler implements EventHandler {
+    @Override
+    public Response handleEvent(Context context, Event event) {
+       String body = new String(event.getBody());
+       String reversed = new StringBuilder(body).reverse().toString();
+
+       return new Response().setBody(reversed.getBytes());
+    }
+}
