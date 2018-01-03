57d12a134d8226805e92c938e3497b659222c693
Miki Tebeka <miki.tebeka@gmail.com>
2018-01-03 16:25:19 +0200

    gradle and user interface

--- java-handler/build.sbt
+++
@@ -1,8 +0,0 @@
-name := "nuclio-test-handler"
-organization := "io.nuclio"
-version := "1.0-SNAPSHOT"
-description := "Nuclio Test Java Handler"
-crossPaths := false
-autoScalaLibrary := false
-
-libraryDependencies += "io.nuclio" % "nuclio-sdk" % "1.0-SNAPSHOT" from "file://" + baseDirectory.value + "/nuclio-sdk-1.0-SNAPSHOT.jar"
--- java-handler/src/main/java/ReverseEventHandler.java
+++ java-handler/src/main/java/ReverseEventHandler.java
@@ -8,8 +8,10 @@ public class ReverseEventHandler implements EventHandler {
     @Override
     public Response handleEvent(Context context, Event event) {
        String body = new String(event.getBody());
+
+       context.getLogger().infoWith("Got event", "body", body);
        String reversed = new StringBuilder(body).reverse().toString();
 
-       return new Response().setBody(reversed.getBytes());
+       return new Response().setBody(reversed);
     }
 }
