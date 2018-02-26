4672ad5a66f83d45714c80f3fc7c7e9d39abba31
Miki Tebeka <miki.tebeka@gmail.com>
2018-02-26 11:46:39 +0200

    meta -> metadata

--- python-handler/handler.py
+++ python-handler/handler.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 def reverser(context, event):
     body = event.body.decode('utf-8')
     return body[::-1]
@@ -5,3 +7,8 @@ def reverser(context, event):
 
 def handler(context, event):
     return 'OK'
+
+
+def slow(context, event):
+    sleep(0.1)
+    return 'SLOW'
