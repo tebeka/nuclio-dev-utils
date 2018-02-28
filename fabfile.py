d882b5c57b8f53f4bed38c40a82af872de0fef95
Miki Tebeka <miki.tebeka@gmail.com>
2018-02-28 22:04:25 +0200

    gce

--- fabfile.py
+++ fabfile.py
@@ -8,18 +8,19 @@ To run tests run
     fab test
 """
 from os import environ
-from os.path import expanduser
+# from os.path import expanduser
 
 from fabric.api import env, execute, run, settings, sudo, task
 from fabric.context_managers import shell_env
 
-ci_host = environ.get('NUCLIO_CI_HOST', '35.153.142.27')
-key_file = environ.get('NUCLIO_CI_KEY', expanduser('~/.ssh/nuclio-ci.pem'))
+ci_host = environ.get('NUCLIO_CI_HOST', '35.196.144.192')
+# key_file = environ.get('NUCLIO_CI_KEY', expanduser('~/.ssh/nuclio-ci.pem'))
 
-user = 'ubuntu'
+# user = 'ubuntu'
+user = 'miki'
 env['user'] = user
 env['host_string'] = ci_host
-env['key_filename'] = key_file
+# env['key_filename'] = key_file
 src_dir = '~/go/src/github.com/nuclio/nuclio'
 
 
@@ -32,6 +33,7 @@ def install_docker():
         'curl',
         'make',
         'software-properties-common',
+        'tmux',
     ]
     sudo(f'apt-get install -y {" ".join(packages)}')
     run('curl -o key -fsSL https://download.docker.com/linux/ubuntu/gpg')
@@ -39,6 +41,29 @@ def install_docker():
     run('rm key')
     version = str(run('lsb_release -cs'))
     url = 'https://download.docker.com/linux/ubuntu'
+    repo = f'deb [arch=amd64] {url} {version} stable'
+    sudo(f'sudo add-apt-repository "{repo}"')
+    sudo('apt-get update')
+    sudo('apt-get install -y docker-ce')
+    sudo('groupadd docker', warn_only=True)
+    sudo(f'usermod -aG docker {user}')
+
+@task
+def install_docker_gce():
+    sudo('apt-get update')
+    packages = [
+        'apt-transport-https',
+        'ca-certificates',
+        'curl',
+        'make',
+        'software-properties-common',
+    ]
+    sudo(f'apt-get install -y {" ".join(packages)}')
+    run('curl -o key -fsSL https://download.docker.com/linux/debian/gpg')
+    sudo('apt-key add key')
+    run('rm key')
+    version = str(run('lsb_release -cs'))
+    url = 'https://download.docker.com/linux/debian'
     repo = f'deb [arch=amd64] {url} {version} stable'
     sudo(f'sudo add-apt-repository "{repo}"')
     sudo('apt-get update')
@@ -59,6 +84,9 @@ def install_go():
 @task
 def clone_nuclio():
     run(f'git clone https://github.com/tebeka/nuclio.git {src_dir}')
+    with settings(cwd=src_dir), shell_env(GOPATH=f'/home/{user}/go'):
+        run('git remote add nuclio https://github.com/nuclio/nuclio.git')
+        run('git fetch nuclio')
     packages = [
         'github.com/nuclio/amqp',
         'github.com/nuclio/logger',
