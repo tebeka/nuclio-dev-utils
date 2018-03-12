4679b2d2a6a87034c917ab071641f4c09f5e0662
Miki Tebeka <miki.tebeka@gmail.com>
2018-03-12 15:37:33 +0200

    two partitions

--- python-handler/handler.py
+++ python-handler/handler.py
@@ -1,4 +1,5 @@
-from time import sleep
+from time import sleep, strftime
+
 
 def reverser(context, event):
     body = event.body.decode('utf-8')
@@ -12,3 +13,12 @@ def handler(context, event):
 def slow(context, event):
     sleep(0.1)
     return 'SLOW'
+
+
+def log(context, event):
+    body = event.body.decode('utf-8')
+    with open('/tmp/py-handler.log', 'at') as out:
+        timestamp = strftime('%Y-%m-%dT%H:%M:%S')
+        out.write('[{}] {}\n'.format(timestamp, body))
+        out.flush()
+    return 'Event logged\n'
